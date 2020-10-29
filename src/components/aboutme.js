3c6c507494b522b91ab2a320d0f57db45d8e3b3f
Wilson Ruan <wilsonruan23@gmail.com>
2020-10-29 15:40:51 -0400

    Ints the images and website links

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Cell, Grid } from 'react-mdl';
+import Wilson from './Wilson-Ruan.jpg'
 
 class Landing extends Component {
   render() {
@@ -7,7 +8,7 @@ class Landing extends Component {
       <div style={{ width: '100%', maring: 'auto' }}>
         <Grid className='landing-grid'>
           <Cell col={6}>
-            <img src='https://i.redd.it/hww8g804tp8z.jpg' alt='avatar' />
+            <img src={Wilson} alt='avatar' />
             <p col={1}>
               I am a Full Stack Developer with a passion of creative ideas, visually engaging, and fully responsive applications. Programming allows me to engage both the creative and logical sides of my brain, and I love the satisfying feeling of bringing new ideas to coding.
               My skills include JavaScript, ES6, jQuery, React/React Hooks, Redux, React Native, Node, Express, MySQL, Sequelize.js MongoDB, Mongoose.js, REST APIs, HTML5, Handlebars, CSS3, Bootstrap, Material UI, Git, and GitHub. I am constantly learning new techniques and collaborating with other developers to become a better Full Stack Developer.
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import { Cell, Grid, Card, CardTitle, CardText, CardActions, Button } from 'react-mdl';
+import RocketShooter from './Rocket-Shooter.jpg'
+import CodeQuiz from './Code-Quiz.jpg'
+import DayPlanner from './Day-Planner.jpg'
+import FlightSearch from './Flight-Search.png'
+import PasswordGenerator from './Password-Generator.jpg'
+import WeatherDashboard from './Weather-Dashboard.jpg'
 
 class Project extends Component {
   constructor(props) {
@@ -12,76 +18,70 @@ class Project extends Component {
       <div className='category-tabs'>
         <Grid>
           <Cell col={12}>
-            <div className='content'>        
-            <div className='project-grid'>
-              <Card shadow={0} style={{ width: '320px', height: '320px', margin: '75px' }}>
-                <CardTitle expand style={{ color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }}>Update</CardTitle>
-                <CardText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenan convallis.
+            <div className='content'>
+              <div className='project-grid'>
+                <Card shadow={0} style={{ width: '320px', height: '320px', margin: '75px' }}>
+                  <CardTitle expand style={{ color: '#fff', background: `url(${RocketShooter}) no-repeat`, backgroundSize: '320px 225px' }}></CardTitle>
+                  <CardText>
+                    Rocket Shooter
                 </CardText>
-                <CardActions border style={{display: 'flex', justifyContent: 'space-around'}}>
-                  <Button>GITHUB</Button>
-                  <Button>WEBSITE</Button>
-                </CardActions>
-              </Card>
-              <Card shadow={0} style={{ width: '320px', height: '320px', margin: '75px' }}>
-                <CardTitle expand style={{ color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }}>Update</CardTitle>
-                <CardText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenan convallis.
+                  <CardActions border style={{ display: 'flex', justifyContent: 'space-around' }}>
+                    <Button href='https://scratch.mit.edu/projects/366012034' target="_blank">GITHUB</Button>
+                    <Button href='https://scratch.mit.edu/projects/366012034' target="_blank"> WEBSITE</Button>
+                  </CardActions>
+                </Card>
+                <Card shadow={0} style={{ width: '320px', height: '320px', margin: '75px' }}>
+                  <CardTitle expand style={{ color: '#fff', background: `url(${WeatherDashboard}) no-repeat`, backgroundSize: '320px 225px' }}></CardTitle>
+                  <CardText>
+                    Weather Dashboard
                 </CardText>
-                <CardActions border style={{display: 'flex', justifyContent: 'space-around'}}>
-                  <Button colored>GITHUB</Button>
-                  <Button colored>WEBSITE</Button>
-                </CardActions>
-              </Card>
-              <Card shadow={0} style={{ width: '320px', height: '320px', margin: '75px' }}>
-                <CardTitle expand style={{ color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }}>Update</CardTitle>
-                <CardText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenan convallis.
+                  <CardActions border style={{ display: 'flex', justifyContent: 'space-around' }}>
+                    <Button href='https://github.com/Wilsonruan/6-Weather-Dashboard' target="_blank">GITHUB</Button>
+                    <Button href='https://wilsonruan.github.io/6-Weather-Dashboard/' target="_blank"> WEBSITE</Button>
+                  </CardActions>
+                </Card>
+                <Card shadow={0} style={{ width: '320px', height: '320px', margin: '75px' }}>
+                  <CardTitle expand style={{ color: '#fff', background: `url(${PasswordGenerator}) no-repeat`, backgroundSize: '320px 225px' }}></CardTitle>
+                  <CardText>
+                    Password Generator
                 </CardText>
-                <CardActions border style={{display: 'flex', justifyContent: 'space-around'}}>
-                  <Button colored>GITHUB</Button>
-                  <Button colored>WEBSITE</Button>
-                </CardActions>
-              </Card>
-              <hr></hr>
-              <Card shadow={0} style={{ width: '320px', height: '320px', margin: '75px' }}>
-                <CardTitle expand style={{ color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }}>Update</CardTitle>
-                <CardText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenan convallis.
+                  <CardActions border style={{ display: 'flex', justifyContent: 'space-around' }}>
+                    <Button href='https://github.com/Wilsonruan/3-Password-Generator' target="_blank">GITHUB</Button>
+                    <Button href='https://wilsonruan.github.io/3-Password-Generator/' target="_blank"> WEBSITE</Button>
+                  </CardActions>
+                </Card>
+                <hr></hr>
+                <Card shadow={0} style={{ width: '320px', height: '320px', margin: '75px' }}>
+                  <CardTitle expand style={{ color: '#fff', background: `url(${FlightSearch}) no-repeat`, backgroundSize: '320px 225px' }}></CardTitle>
+                  <CardText>
+                    Flight Search
                 </CardText>
-                <CardActions border style={{display: 'flex', justifyContent: 'space-around'}}>
-                  <Button colored>GITHUB</Button>
-                  <Button colored>WEBSITE</Button>
-                </CardActions>
-              </Card>
-              <Card shadow={0} style={{ width: '320px', height: '320px', margin: '75px' }}>
-                <CardTitle expand style={{ color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }}>Update</CardTitle>
-                <CardText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenan convallis.
+                  <CardActions border style={{ display: 'flex', justifyContent: 'space-around' }}>
+                    <Button href='https://github.com/Wilsonruan/Flight-Search' target="_blank">GITHUB</Button>
+                    <Button href='https://wilsonruan.github.io/Flight-Search/' target="_blank"> WEBSITE</Button>
+                  </CardActions>
+                </Card>
+                <Card shadow={0} style={{ width: '320px', height: '320px', margin: '75px' }}>
+                  <CardTitle expand style={{ color: '#fff', background: `url(${DayPlanner}) no-repeat`, backgroundSize: '320px 225px' }}></CardTitle>
+                  <CardText>
+                    Day Planner
                 </CardText>
-                <CardActions border style={{display: 'flex', justifyContent: 'space-around'}}>
-                  <Button colored>GITHUB</Button>
-                  <Button colored>WEBSITE</Button>
-                </CardActions>
-              </Card>
-              <Card shadow={0} style={{ width: '320px', height: '320px', margin: '75px' }}>
-                <CardTitle expand style={{ color: '#fff', background: 'url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC' }}>Update</CardTitle>
-                <CardText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenan convallis.
+                  <CardActions border style={{ display: 'flex', justifyContent: 'space-around' }}>
+                    <Button href='https://github.com/Wilsonruan/5-Day-Planner' target="_blank">GITHUB</Button>
+                    <Button href='https://wilsonruan.github.io/5-Day-Planner/' target="_blank"> WEBSITE</Button>
+                  </CardActions>
+                </Card>
+                <Card shadow={0} style={{ width: '320px', height: '320px', margin: '75px' }}>
+                  <CardTitle expand style={{ color: '#fff', background: `url(${CodeQuiz}) no-repeat`, backgroundSize: '320px 225px' }}></CardTitle>
+                  <CardText>
+                    Code Quiz
                 </CardText>
-                <CardActions border style={{display: 'flex', justifyContent: 'space-around'}}>
-                  <Button colored>GITHUB</Button>
-                  <Button colored>WEBSITE</Button>
-                </CardActions>
-              </Card>
-            </div></div>
+                  <CardActions border style={{ display: 'flex', justifyContent: 'space-around' }}>
+                    <Button href='https://github.com/Wilsonruan/4-Code-Quiz' target="_blank">GITHUB</Button>
+                    <Button href='https://wilsonruan.github.io/4-Code-Quiz/' target="_blank"> WEBSITE</Button>
+                  </CardActions>
+                </Card>
+              </div></div>
           </Cell>
         </Grid>
       </div>
