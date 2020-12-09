a0ef1ebe3deabb5cdaffd2f6554bd2c9e0461260
Wilson Ruan <wilsonruan23@gmail.com>
2020-12-09 14:04:59 -0500

    update email

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -15,7 +15,7 @@ export default function header() {
           <Breadcrumb.Item href="https://drive.google.com/file/d/1RTGhtVr1cGEi1Ku8t9dG9lptUcOKfEWU/view?usp=sharing" target="_blank">Resume</Breadcrumb.Item>
           <Nav.Link href="https://github.com/Wilsonruan" target="_blank"><i style={{ marginRight: "-15px" }} class="fa fa-github fa-2x" aria-hidden="true"></i></Nav.Link>
           <Nav.Link href="mailto:wilsonruan23@gmail.com" target="_blank"><i style={{ marginRight: "-15px" }} class="fa fa-envelope fa-2x"></i></Nav.Link>
-          <Nav.Link href="https://www.linkedin.com/in/wilson-ruan-874069139/" target="_blank"><i style={{ marginRight: "-15px" }} class="fa fa-linkedin fa-2x"></i></Nav.Link>
+          <Nav.Link href="https://www.linkedin.com/in/wilsonruan/" target="_blank"><i style={{ marginRight: "-15px" }} class="fa fa-linkedin fa-2x"></i></Nav.Link>
         </Navbar.Collapse> 
       </Container>
     </Navbar>
