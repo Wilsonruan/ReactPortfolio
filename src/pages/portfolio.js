46b135cddcf48c73b0758da3ac5c1d1147132225
Wilson Ruan <wilsonruan23@gmail.com>
2021-01-10 16:13:07 -0500

    Inits shopsmart

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -5,18 +5,19 @@ import project from "../projects.json"
 import Budget from "../images/Budget-Trackers.jpg"
 import Employee from "../images/Employee-Directory.jpg"
 import RocketShooter from "../images/Code-Quiz.jpg"
-import ShopSmart from "../images/ShopSmartSS.png"
+import ShopSmart from "../images/ShopSmart.jpg"
 import Google from "../images/Google-Books.jpg"
 import FlightSearch from "../images/Flight-Search.png"
 import NoteTaker from "../images/Note-Taker.jpg"
 import WorkOut from "../images/Workout-Tracker.jpg"
+
 
 class projects extends Component {
   state = {
     project
 }
   render() {
-    console.log(WorkOut)
+    console.log(ShopSmart)
     return (
       <Container style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', marginTop: "100px" }}>
         {this.state.project.map(value => (
