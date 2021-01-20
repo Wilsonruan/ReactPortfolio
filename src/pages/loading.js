160008c43ae06649d1a8e708b89a77878a0359cc
Wilson Ruan <wilsonruan23@gmail.com>
2021-01-19 21:49:25 -0500

    center loading image

--- src/pages/loading.js
+++ src/pages/loading.js
@@ -2,13 +2,7 @@ import React from 'react'
 
 export default function LoadingBox() {
   return (
-    <div className='loading'>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+    <div style={{ textAlign: 'center', marginTop: '300px', fontSize: '48px' }}>
       <i className='fa fa-spinner fa-spin'></i> Loading...
     </div>
   )
