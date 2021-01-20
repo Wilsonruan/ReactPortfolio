c401321533f461a4a0666d5fce98517d7947f58f
Wilson Ruan <wilsonruan23@gmail.com>
2021-01-19 22:05:58 -0500

    adust time

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 6000)
+    setTimeout(() => setLoading(false), 1000)
   }, [])
   
   return (
