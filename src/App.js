ca68ae1cb30d0757aedd82be19b0677d747633be
Wilson Ruan <wilsonruan23@gmail.com>
2021-04-21 23:38:42 -0400

    remove loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React, { useState, useEffect } from 'react'
 import './App.css';
 import { Container } from "react-bootstrap";
 import Header from './components/Header/header'
 import Main from './pages/main'
 import Footer from './components/Footer/footer'
-import LoadingScreen from './loading'
 
 
 function App() {
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1000)
-  }, [])
   
   return (
     <>
       <Header />
-        <LoadingScreen />
+        <Container>
+          <Main />
+        </Container>
       <Footer />
     </>
   );
