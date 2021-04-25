6df644dd9176ccfec380761e0709905dcf79205b
Wilson Ruan <wilsonruan23@gmail.com>
2021-04-25 16:33:58 -0400

    inits 3 more projects.

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,6 +3,9 @@ import { Container } from "react-bootstrap";
 import Project from "../components/Projects/projects"
 import project from "../projects.json"
 import Budget from "../images/Budget-Trackers.jpg"
+import Employee from "../images/Employee-Directory.jpg"
+import Password from "../images/Password-Generator.jpg"
+import Weather from "../images/Day-Planner.jpg"
 import Portfolio from "../images/Portfolio.jpg"
 import CodeQuiz from "../images/Code-Quiz.jpg"
 import ShopSmart from "../images/ShopSmart.jpg"
@@ -16,7 +19,7 @@ class projects extends Component {
     project
 }
   render() {
-    console.log(Portfolio)
+    console.log(Weather)
     return (
       <Container style={{ background: "white", display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', marginTop: "100px", marginBottom: "100px", paddingTop: "50px", boxShadow: '5px 10px 18px #888888' }}>
         {this.state.project.map(value => (
