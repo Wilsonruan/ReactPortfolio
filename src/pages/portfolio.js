9d3ec30db7b12661ac48fa29257c41aca70bf11e
Wilson Ruan <wilsonruan23@gmail.com>
2021-04-25 15:55:14 -0400

    change to portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,13 +3,11 @@ import { Container } from "react-bootstrap";
 import Project from "../components/Projects/projects"
 import project from "../projects.json"
 import Budget from "../images/Budget-Trackers.jpg"
-import Employee from "../images/Employee-Directory.jpg"
+import Portfolio from "../images/Portfolio.jpg"
 import CodeQuiz from "../images/Code-Quiz.jpg"
 import ShopSmart from "../images/ShopSmart.jpg"
-import Google from "../images/Google-Books.jpg"
 import FlightSearch from "../images/Flight-Search.png"
 import NoteTaker from "../images/Note-Taker.jpg"
-import WorkOut from "../images/Workout-Tracker.jpg"
 import Scratch from "../components/Scratch/scratch"
 import Instructions from "../components/Instructions/instructions"
 
@@ -18,7 +16,7 @@ class projects extends Component {
     project
 }
   render() {
-    console.log(CodeQuiz)
+    console.log(Portfolio)
     return (
       <Container style={{ background: "white", display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', marginTop: "100px", marginBottom: "100px", paddingTop: "50px", boxShadow: '5px 10px 18px #888888' }}>
         {this.state.project.map(value => (
