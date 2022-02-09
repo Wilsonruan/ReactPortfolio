841171b4d2aeb7257099425604c75ca2f906bfcf
Wilson <wruan@tvo.org>
2022-02-09 08:05:35 -0500

    change projects

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -6,6 +6,7 @@ import Budget from "../images/Budget-Trackers.jpg"
 import Employee from "../images/Employee-Directory.jpg"
 import Password from "../images/Password-Generator.jpg"
 import Weather from "../images/Day-Planner.jpg"
+import Weathers from "../images/Weather-Dashboard.jpg"
 import Portfolio from "../images/Portfolio.jpg"
 import CodeQuiz from "../images/Code-Quiz.jpg"
 import ShopSmart from "../images/ShopSmart.jpg"
@@ -20,7 +21,7 @@ class projects extends Component {
     project
 }
   render() {
-    console.log(Weather)
+    console.log(Weathers)
     return (
       <Container style={{ background: "white", display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', marginTop: "100px", marginBottom: "100px", paddingTop: "50px", boxShadow: '5px 10px 18px #888888' }}>
         {this.state.project.map(value => (
