dfe42d6177dcb878fd8904c71bb56018e9681e2c
Wilson <wruan@tvo.org>
2022-02-16 23:12:01 -0500

    github 100% online

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -7,12 +7,12 @@ export default function header() {
   return (
     <Navbar fixed="top" id="header" style={{ marginBottom: "50px"}}>
       <Container>
-        <Link className='text-primary' to="/ReactPortfolio/">Wilson Ruan</Link>
+        <Link className='text-primary' to="/">Wilson Ruan</Link>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Link class="mr-3" to="/ReactPortfolio/">Home</Link>
-          <Link class="mr-3" to="/ReactPortfolio/aboutme/">About</Link>
-          <Link class="mr-3" to="/ReactPortfolio/portfolio/">Portfolio</Link>
+          <Link class="mr-3" to="/">Home</Link>
+          <Link class="mr-3" to="/aboutme/">About</Link>
+          <Link class="mr-3" to="/portfolio/">Portfolio</Link>
           <Breadcrumb.Item class="mr-3" href="https://drive.google.com/file/d/197RK4KqoGBqjp0ypGt3cPX-lBromFwWn/view?usp=sharing" target="_blank">Resume</Breadcrumb.Item>
           <Nav.Link href="https://github.com/Wilsonruan" target="_blank"><i style={{ marginRight: "-15px" }} class="fa fa-github fa-2x" aria-hidden="true"></i></Nav.Link>
           <Nav.Link href="mailto:wilsonruan23@gmail.com" target="_blank"><i style={{ marginRight: "-15px" }} class="fa fa-envelope fa-2x"></i></Nav.Link>
