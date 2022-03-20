e704ada27ed7df581c11284b9c75b4c7d4a5d5b2
Wilson <wruan@tvo.org>
2022-03-20 12:11:04 -0400

    fix naming

--- src/components/Scratch/scratch.js
+++ src/components/Scratch/scratch.js
@@ -5,7 +5,7 @@ import './scratch.css'
 export default function scratch() {
   return (
     <Card style={{width: '30.5rem', marginBottom: '100px', boxShadow: '5px 10px 18px #888888'}}>
-      <iframe allowtransparency="true" width="485" height="402" src="https://scratch.mit.edu/projects/366012034/embed/?autostart=true" frameborder="0" allowfullscreen></iframe>
+      <iframe allowtransparency="true" width="485" height="402" src="https://scratch.mit.edu/projects/366012034/embed/?autostart=true" frameBorder="0" allowFullScreen></iframe>
     </Card>
   )
 }
