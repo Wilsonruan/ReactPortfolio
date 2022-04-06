0cfb4e3b02e8398451b74ff147d462bea6fb45c5
Wilson <wruan@tvo.org>
2022-04-06 11:35:24 -0400

    removing code-quiz

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-import { Container } from "react-bootstrap";
 import Header from './components/Header/header'
-import Main from './pages/main'
 import Footer from './components/Footer/footer'
 import Loading from './loading'
 
