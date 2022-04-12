0ab19f39c5f422381ad3d07f35a8fdd32af94ce4
Wilson <wruan@tvo.org>
2022-04-12 09:29:33 -0400

    remove link index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
-  <BrowserRouter basename="/ReactPortfolio/">
+  <BrowserRouter basename="/">
     <App />
   </BrowserRouter>,
   document.getElementById('root')
